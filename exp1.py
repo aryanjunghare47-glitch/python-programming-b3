077e88b4f05877bac69132242f131f2c194cf554
Aryan Junghare <aryanjunghare47@gmail.com>
2026-02-05 11:22:25 +0530

    python programming exp1

---
+++ exp1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 10:50:12 2026
+
+@author: User
+"""
+
+name = input("enter ypur name: ")
+roll_no = input("enter your roll number: ")
+division = input("enter your division: ")
+batch = input ("enter your batch: ")
+college = ("enter your college name: ")
+print("\n----student information----")
+
+print("Name:",name)
+print("Roll number:",roll_no)
+print("Division:",division)
+print("Batch:",batch)
+print("College:",college)

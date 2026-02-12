6201b0d810eec469dca7461c5a956e3d240982ce
Aryan Junghare <aryanjunghare47@gmail.com>
2026-02-12 10:30:01 +0530

    python programming exp2 leap year

---
+++ exp2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 09:59:45 2026
+
+@author: User
+"""
+year = int(input("Enter year: ")) 
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0): 
+  print("Leap Year") 
+else: 
+  print("Not a Leap Year")
